d1d33b653c30961d1abc4b29e6aa1c9e7d69541d
Dan Liebster <dliebster@illuminate.solutions>
2023-09-18 15:40:14 -0400

    structs

--- course/3-functions/exercises/4-pass_by_value/code.go
+++ course/3-functions/exercises/4-pass_by_value/code.go
@@ -7,8 +7,11 @@ func main() {
 	const sendsToAdd = 25
 	incrementSends(sendsSoFar, sendsToAdd)
 	fmt.Println("you've sent", sendsSoFar, "messages")
+	fmt.Println("Betch were looking for", incrementSends(sendsSoFar, sendsToAdd))
+	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
+	fmt.Println("Much Better!! ", sendsSoFar)
 }
 
 func incrementSends(sendsSoFar, sendsToAdd int) int {
-	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar + sendsToAdd
 }
